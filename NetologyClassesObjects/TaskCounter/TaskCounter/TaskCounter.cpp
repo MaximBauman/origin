f7eb801bc907c1363afd632af531c94f1a338974
m.bauman <m.bauman@DT609RIH>
2024-08-22 18:32:45 +0300

    add Task Counter

---
+++ NetologyClassesObjects/TaskCounter/TaskCounter/TaskCounter.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+class Counter
+{
+public:
+	Counter() : n_counter(1) {};
+
+	void set_counter(int num) {
+		this->n_counter = num;
+	};
+	void set_increment() {
+		n_counter++;
+	}
+	void set_decrement() {
+		n_counter--;
+	}
+	void get_actualValue() {
+		std::cout << n_counter << std::endl;
+	}
+
+
+private:
+	int n_counter;
+};
+
+
+int main()
+{
+	system("chcp 1251");
+	setlocale(LC_ALL, "Russian");
+
+	int userNumber;
+	std::string userAction;
+	char symbol;
+
+	Counter counter;
+	std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+	std::cin >> userAction;
+
+	if (userAction == "да") {
+		std::cout << "Введите начальное значение счётчика: ";
+		std::cin >> userNumber;
+		
+		counter.set_counter(userNumber);
+	}
+	std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+	std::cin >> symbol;
+
+	while (!(symbol == 'x' || symbol == 'х')){
+		if (symbol == '+') {
+			counter.set_increment();
+		}
+		else if (symbol == '-') {
+			counter.set_decrement();
+		}
+		else if (symbol == '=') {
+			counter.get_actualValue();
+		}
+			std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+			std::cin >> symbol;
+		};
+	std::cout << "До свидания!";
+}
+
+
