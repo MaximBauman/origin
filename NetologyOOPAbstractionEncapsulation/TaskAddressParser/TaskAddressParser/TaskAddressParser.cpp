c366ffa5e9b305b664d0e550cad2d0efb9bdfceb
m.bauman <m.bauman@DT609RIH>
2024-08-28 15:44:53 +0300

    add new TaskParser

--- NetologyOOPAbstractionEncapsulation/TaskAddressParser/TaskAddressParser/TaskAddressParser.cpp
+++ NetologyOOPAbstractionEncapsulation/TaskAddressParser/TaskAddressParser/TaskAddressParser.cpp
@@ -1,20 +1,76 @@
-// TaskAddressParser.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class Address {
+public:
+    Address(const std::string& city, const std::string& street, int house, int apartment)
+        : city(city), street(street), house(house), apartment(apartment) {}
+
+    std::string get_output_address() const {
+        return city + ", " + street + ", " + std::to_string(house) + ", " + std::to_string(apartment);
+    }
+
+    std::string get_city() {
+        return city;
+    }
+
+
+
+private:
+    std::string city;
+    std::string street;
+    int house;
+    int apartment;
+};
+
+int main() {
+    system("chcp 1251");
+    setlocale(LC_ALL, "Russian");
+
+    int rows;
+    std::string city;
+    std::string street;
+    int house;
+    int apartment;
+
+    std::ifstream inputFile("in.txt");
+    std::ofstream outputFile("out.txt");
+
+
+    if (!inputFile.is_open()) {
+        std::cout << "Can't open file" << std::endl;
+        return 1;
+    }
+
+    inputFile >> rows;
+    std::vector<Address> address;
+
+    for (int i = 0; i < rows; i++) {
+        inputFile >> city >> street >> house >> apartment;
+        address.emplace_back(city, street, house, apartment);
+    }
+
+    for (int i = 0; i < rows - 1; i++) {
+        for (int j = 0; j < rows - i - 1; j++) {
+            if (address[j].get_city() > address[j + 1].get_city()) {
+                std::swap(address[j], address[j + 1]);
+            }
+        }
+    }
+
+    if (!outputFile.is_open()) {
+        std::cout << "Can't open output file!" << std::endl;
+        return 1;
+    }
+    outputFile << rows << std::endl;
+
+    for (int i = 0; i < rows; i++) {
+        outputFile << address[i].get_output_address() << std::endl;
+    }
+
+    inputFile.close();
+    outputFile.close();
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
