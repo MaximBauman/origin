6a02f2ffeb2ae3b464d77c7086161b82f2834923
m.bauman <m.bauman@DT609RIH>
2025-03-31 17:05:40 +0300

    add task with unit test (catch2 framework)

---
+++ NetologyAdvancedProg/Catch2/Test_doubly_linked_list/Test_doubly_linked_list.cpp
@@ -0,0 +1,124 @@
+#include "catch_amalgamated.hpp"
+#include <iostream>
+
+struct ListNode
+{
+public:
+    ListNode(int value, ListNode* prev = nullptr, ListNode* next = nullptr)
+        : value(value), prev(prev), next(next)
+    {
+        if (prev != nullptr) prev->next = this;
+        if (next != nullptr) next->prev = this;
+    }
+
+public:
+    int value;
+    ListNode* prev;
+    ListNode* next;
+};
+
+
+class List
+{
+public:
+    List()
+        : m_head(new ListNode(static_cast<int>(0))), m_size(0),
+        m_tail(new ListNode(0, m_head))
+    {
+    }
+
+    virtual ~List()
+    {
+        Clear();
+        delete m_head;
+        delete m_tail;
+    }
+
+    bool Empty() { return m_size == 0; }
+
+    unsigned long Size() { return m_size; }
+
+    void PushFront(int value)
+    {
+        new ListNode(value, m_head, m_head->next);
+        ++m_size;
+    }
+
+    void PushBack(int value)
+    {
+        new ListNode(value, m_tail->prev, m_tail);
+        ++m_size;
+    }
+
+    int PopFront()
+    {
+        if (Empty()) throw std::runtime_error("list is empty");
+        auto node = extractPrev(m_head->next->next);
+        int ret = node->value;
+        delete node;
+        return ret;
+    }
+
+    int PopBack()
+    {
+        if (Empty()) throw std::runtime_error("list is empty");
+        auto node = extractPrev(m_tail);
+        int ret = node->value;
+        delete node;
+        return ret;
+    }
+
+    void Clear()
+    {
+        auto current = m_head->next;
+        while (current != m_tail)
+        {
+            current = current->next;
+            delete extractPrev(current);
+        }
+    }
+
+private:
+    ListNode* extractPrev(ListNode* node)
+    {
+        auto target = node->prev;
+        target->prev->next = target->next;
+        target->next->prev = target->prev;
+        --m_size;
+        return target;
+    }
+
+private:
+    ListNode* m_head;
+    ListNode* m_tail;
+    unsigned long m_size;
+};
+
+TEST_CASE("doubly_linked", "[linked list]") {
+    List new_list;
+    SECTION("Empty function") {
+        INFO("Empty function");
+        CHECK(new_list.Empty() == true);
+        INFO("Empty fucntion (wrong test answ)");
+        CHECK(new_list.Empty() == false);
+
+    }
+    SECTION("Size function") {
+        new_list.PushFront(12);
+        new_list.PushBack(21);
+        INFO("Size function");
+        CHECK(new_list.Size() == 2);
+ 
+    }
+    SECTION("Clear function") {
+        new_list.Clear();
+        INFO("Clear func");
+        CHECK(new_list.Empty() == true);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    
+    return Catch::Session().run(argc, argv);
+}
+
