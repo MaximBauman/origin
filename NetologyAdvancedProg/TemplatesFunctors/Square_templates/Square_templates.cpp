838ac017f656dfe00d7c8ff80780db92461ce205
m.bauman <m.bauman@DT609RIH>
2025-04-10 14:55:50 +0300

    bug fixed

--- NetologyAdvancedProg/TemplatesFunctors/Square_templates/Square_templates.cpp
+++ NetologyAdvancedProg/TemplatesFunctors/Square_templates/Square_templates.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 template<typename T>
-T squaring(T a) {
+auto squaring(T a) {
 	return a * a;
 }
 template< typename T2>
@@ -12,24 +12,30 @@ void vector_squaring(std::vector<T2>& vec) {
 	}
 	std::cout << std::endl;
 }
+
+template<typename T3>
+void ReadInput(T3& value) {
+	std::cin >> value;
+}
+
 int main()
 {
-	int input = 0;
+	auto input = 0.0;
 	std::cout << "Hello, it's square template function." << std::endl; 
 	do {
 		std::cout << "1. Enter an element 2. Enter an array of elements. If you want to finish entering, enter -999 \r" << std::endl;
 		std::cin >> input;
 		if (input == 1) {
 			std::cout << "Input element: ";
-			std::cin >> input;
+			ReadInput(input);
 			std::cout << squaring(input) << std::endl;
 		}
 		else if(input == 2) {
 			std::cout << "Enter an array of elements. If you want to finish entering, enter -999: ";
 			std::vector<int>numbers;
 			 while (input != -999) {
-				std::cin >> input;
-				if (input != -999) {
+				 ReadInput(input);
+				 if (input != -999) {
 					numbers.push_back(input);
 				}
 				else {
@@ -43,6 +49,11 @@ int main()
 		else { continue; }
 		
 	} while (input != -999);
+
+	std::vector<double> vector_double{ 1.68, 2.5, 10.12 };
+	std::vector<int> vector_int{ 1, 2, 10 };
+	vector_squaring(vector_double);
+	vector_squaring(vector_int);
 
 	return 0;
 }
--- NetologyAdvancedProg/TemplatesFunctors/table_template/table_template.cpp
+++ NetologyAdvancedProg/TemplatesFunctors/table_template/table_template.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 
-template<typename T>
+template<typename T, typename T2>
 
 class Table {
 protected:
     T _rows_, _columns_;
-    T** arr;
+    T2** arr;
 public: 
     Table(T rows, T columns) : _rows_(rows), _columns_(columns) {
-        arr = new T * [rows];
+        arr = new T2 * [rows];
         for (auto i = 0; i < rows; i++) {
-            arr[i] = new T[columns];
+            arr[i] = new T2[columns];
         };
     };
 
     void print() {
         std::cout << _rows_ << _columns_ << std::endl;
     };
-    T* operator[] (T i) const{
+    T2* operator[] (T i) const{
         return arr[i];
     };
-    T* operator[](T i) {
+    T2* operator[](T i) {
         return arr[i];
     };
     T Size() const {
@@ -36,13 +36,26 @@ public:
 
 int main()
 {
-    Table table(2, 3); //For C++ 17+ standart, if using earlier Table<input types> name(num1, num2);
+    Table<int, int> table(2, 3); //For C++ 17+ standart, if using earlier Table<input types> name(num1, num2);
     std::cout << table.Size() << std::endl;
-    table[1][2] = 10.5;
+    table[1][2] = 10;
     table[0][0] = 4;
     std::cout << table[0][0] << " ";
-    std::cout << table[1][2] << " ";
+    std::cout << table[1][2] << " " << std::endl;
 
+    Table<int, double> table2(2, 3); //For C++ 17+ standart, if using earlier Table<input types> name(num1, num2);
+    std::cout << table2.Size() << std::endl;
+    table2[1][2] = 10.5;
+    table2[0][0] = 4;
+    std::cout << table2[0][0] << " ";
+    std::cout << table2[1][2] << " " << std::endl;
+
+    Table<int, float> table3(2, 3); //For C++ 17+ standart, if using earlier Table<input types> name(num1, num2);
+    std::cout << table3.Size() << std::endl;
+    table3[1][2] = 10.5231;
+    table3[0][0] = 4.321;
+    std::cout << table3[0][0] << " ";
+    std::cout << table3[1][2] << " " << std::endl;
 
     return 0;
 }
