1254e682c3aa4d1790cb35e0151b9300b970030d
Home PC Max Bauman <m.bauman.x@gmail.com>
2025-04-21 17:54:47 +0300

    add new homeworks

---
+++ NetologyAdvancedProg/STL_lib/part_2/Container_print/Container_print/Container_print.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <set>
+#include <list>
+#include <vector>
+
+
+
+template<typename T>
+
+void print_container(T cont) {
+    std::cout << "Your container: ";
+    for (auto i : cont) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+
+    print_container(test_set);
+    print_container(test_list);
+    print_container(test_vector);
+
+    return 0;
+}
+
+
---
+++ NetologyAdvancedProg/STL_lib/part_2/Unique_vector/Unique_vector/Unique_vector.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+std::vector<int> vector_filler();
+void duplicate_cleanup(std::vector<int> V);
+
+int main()
+{
+    std::vector<int> V1;
+    std::string again = "continue";
+    std::string exit = "exit";
+    std::string user_string;
+
+    std::cout << "Hi! This is a duplicate cleanup program. " << std::endl; 
+    do{
+        std::vector<int> V1 = vector_filler();
+        duplicate_cleanup(V1);
+        std::cout << "If you won't to input new vector, enter continue, or exit to stop program: ";
+        std::cin >> user_string;
+    } while (user_string != exit);
+
+    return 0;
+}
+
+std::vector<int> vector_filler() {
+    std::vector<int> V;
+    int user_int;
+    int vector_size;
+    std::cout << "First, enter how many numbers you are entering and start entering the numbers for the vector there: ";
+    std::cin >> vector_size;
+    for (auto i = 0; i < vector_size; i++) {
+        std::cin >> user_int;
+        V.push_back(user_int);
+    }
+    std::cout << "Your vector: ";
+    for (auto i : V) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+    return V;
+}
+
+void duplicate_cleanup(std::vector<int> V) {
+    std::sort(V.begin(), V.end());
+    std::cout << "Sorted vector: ";
+    for (auto i : V) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    auto it = std::unique(V.begin(), V.end());
+    V.erase(it, V.end());
+    std::cout << "Vector with out duplicate's: ";
+    for (auto i : V) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+};
