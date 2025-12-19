6c932d213286d8de4435353b912bf8637e64c046
m.bauman <m.bauman.x@gmail.com>
2025-12-19 14:24:18 +0300

    add task with docker

---
+++ NetologyAsync/Docker_hello/Docker_hello.cpp
@@ -0,0 +1,21 @@
+﻿//Docker downloaded 
+//cmd -> docker --version (Docker version 29.1.3)
+//docker pull ubuntu
+//apt update 
+//apt install g++
+//apt install nano
+//nano hello.cpp - > paste lower code -> ctrl + x
+//g++ hello.cpp -o hello
+// ./hello
+// exit
+
+
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n" << std::endl;
+
+}
